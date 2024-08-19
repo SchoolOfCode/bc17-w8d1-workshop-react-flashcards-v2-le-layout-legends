1882e2c6dfd56ed21715f0eda268f83bd621ff81
Richard Lamb <lamby42@live.co.uk>
2024-08-19 11:59:04 +0100

    fiddled with footer

--- src/components/Flashcards/Flashcard/Flashcard.jsx
+++ src/components/Flashcards/Flashcard/Flashcard.jsx
@@ -1,7 +1,7 @@
-export default function Flashcard({ image, alt, title, description }) {
+export default function Flashcard({title, description }) {
 	return (
-		<article className="tile">
-			<img src={image} alt={alt} className="tile-image" />
+		<article className="flashacard">
+	
 			<h3>{title}</h3>
 			<p>{description}</p>
 		</article>
--- src/components/Flashcards/FlashcardsContainer/FlashcardsContainer.jsx
+++ src/components/Flashcards/FlashcardsContainer/FlashcardsContainer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './FlashcardsContainer.module.css'
 
 export default function FlashcardsContainer() {
   return (
