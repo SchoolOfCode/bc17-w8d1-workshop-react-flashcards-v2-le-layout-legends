85b1b0e9f0aa7bf7dace02da4273602336c66d03
clw6444 <clw6444@gmail.com>
2024-08-19 10:29:29 +0000

    Changes to header and app css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
+import FlashcardsContainer from './components/Flashcards/FlashcardsContainer/FlashcardsContainer'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
     <>
       <Header/>
+      <FlashcardsContainer/>
+      <Footer/>
     </>
   )
 }
---
+++ src/components/Flashcards/FlashcardsContainer/FlashcardsContainer.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function FlashcardsContainer() {
+  return (
+    <main>FlashcardsContainer</main>
+  )
+}
